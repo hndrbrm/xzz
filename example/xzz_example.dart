f5a5c11a40cb78d22c1e54b87a703fe1578f5fa8
hndrbrm <hndrbrm@gmail.com>
2024-12-05 08:57:57 +0700

    Use test as an example on how to use it.

--- example/xzz_example.dart
+++ example/xzz_example.dart
@@ -2,15 +2,6 @@
 // All rights reserved. Use of this source code is governed
 // by a BSD-style license that can be found in the LICENSE file.
 
-import 'dart:io';
-
-import 'package:xzz/xzz.dart';
-
 void main() {
-  final file = File('pcb/iPhone4S boardview.pcb');
-  final content = file.readAsBytesSync();
-  final xzz = Xzz.deserialize(content.iterator);
-  final bytes = xzz.serialize();
-  final output = File('output.pcb');
-  output.writeAsBytesSync(bytes);
+  // Look on test to find out how to use it.
 }
--- test/xzz_test.dart
+++ test/xzz_test.dart
@@ -1,16 +1,18 @@
+// Copyright 2024. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed
+// by a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
 import 'package:xzz/xzz.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+  test('Serialize Test', () {
+    final file = File('${Directory.current.path}/test/pcb/iPhone4S boardview.pcb');
+    final content = file.readAsBytesSync();
+    final xzz = Xzz.deserialize(content.iterator);
+    final bytes = xzz.serialize();
+    expect(bytes, equals(content));
   });
 }
