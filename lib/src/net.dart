0dd8803473df96e6a29805c5b168b77faacd7a9c
hndrbrm <hndrbrm@gmail.com>
2024-12-06 06:35:46 +0700

    Refactor in Net file.

--- lib/src/net.dart
+++ lib/src/net.dart
@@ -11,28 +11,28 @@ import 'shareable/serializer.dart';
 
 final class Net implements Serializer {
   Net._({
-    required this.order,
+    required this.index,
     required this.name,
   });
 
   factory Net.deserialize(Iterator<int> iterator) {
     final length = iterator.read(4).toUint32();
-    final order = iterator.read(4).toUint32();
+    final index = iterator.read(4).toUint32();
     final name = iterator.read(length - 8).toString8();
 
     return Net._(
-      order: order,
+      index: index,
       name: name,
     );
   }
 
-  final int order;
+  final int index;
   final String name;
 
   @override
   List<int> serialize() => [
     ...(name.length + 8).toUint32List(),
-    ...order.toUint32List(),
+    ...index.toUint32List(),
     ...utf8.encode(name),
   ];
 }
